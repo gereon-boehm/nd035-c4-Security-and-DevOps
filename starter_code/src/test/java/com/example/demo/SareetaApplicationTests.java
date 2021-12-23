c63c4ef64672cdeb2b93b1d70416c6e90ff4eb0c
Gereon Böhm <boehm@fzi.de>
2021-12-23 14:07:55 +0100

    Enable Authentication using Spring Security with JWT

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@EnableJpaRepositories("com.example.demo.model.persistence.repositories")
+@EntityScan("com.example.demo.model.persistence")
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SareetaApplicationTests {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	@Test
 	public void contextLoads() {
